be8ccb2e2bde0b82108d8e59004701672b1edc04
mint <mintchipleaf@gmail.com>
2014-04-27 02:59:22 -0400

    Add ball, game over/restart, points, ball collision

--- ludum.js
+++ ludum.js
@@ -6,7 +6,8 @@ var manifest = {
 		"square1":"images/square1.png",
 		"square2":"images/square2.png",
 		"square3":"images/square3.png",
-		"square4":"images/square4.png"
+		"square4":"images/square4.png",
+		"ball":"images/ball.png"
 	},
 	"sounds": {
 	},
@@ -18,97 +19,120 @@ var manifest = {
 
 var game = new Splat.Game(canvas, manifest);
 
+var paddle;
+var entity;
+var gameOver = false;
 var score = 0;
-var row1 = [4,4,4,4,4,4,4,4,4,4];
+var balls = [];
+var row1 = [];
+var row2 = [];
+var row3 = [];
 var keys1 = ["q","w","e","r","t","y","u","i","o","p"];
-var row2 = [4,4,4,4,4,4,4,4,4,4];
 var keys2 = ["a","s","d","f","g","h","j","k","l","semicolon"];
-var row3 = [4,4,4,4,4,4,4,4,4,4];
 var keys3 = ["z","x","c","v","b","n","m","comma","period","forwardslash"];
 
+function setHeights(){
+	row1 = [4,4,4,4,4,4,4,4,4,4];
+	row2 = [4,4,4,4,4,4,4,4,4,4];
+	row3 = [4,4,4,4,4,4,4,4,4,4];
+}
 
 function checkKB(){
+	var key;
 	for (var i = 0; i <= keys1.length - 1; i++) {
-
-		if(game.keyboard.consumePressed(keys1[i]) && row1[i] > 0){
-			row1[i] -= 1;
-			console.log(keys1[i] + " " + row1[i]);
-		}if(game.keyboard.consumePressed(keys2[i]) && row2[i] > 0){
-			row2[i] -= 1;
-			console.log(keys2[i] + " " + row2[i]);
-		}if(game.keyboard.consumePressed(keys3[i]) && row3[i] > 0){
-			row3[i] -= 1;
-			console.log(keys3[i] + " " + row3[i]);
+			if(game.keyboard.consumePressed(keys1[i]) && row1[i] > 0){
+				row1[i] -= 1;
+				console.log(keys1[i] + " " + row1[i]);
+				checkCollisions(1,i);
+				key = true;
+			}if(game.keyboard.consumePressed(keys2[i]) && row2[i] > 0){
+				row2[i] -= 1;
+				console.log(keys2[i] + " " + row2[i]);
+				checkCollisions(2,i);
+				key = true;
+			}if(game.keyboard.consumePressed(keys3[i]) && row3[i] > 0){
+				row3[i] -= 1;
+				console.log(keys3[i] + " " + row3[i]);
+				checkCollisions(3,i);
+				key = true;
+			}
+		if(key && gameOver){
+			game.scenes.switchTo("title");
 		}
 	}
 }
 
-game.scenes.add("title", new Splat.Scene(canvas, function() {
-	/*var playerLeftImg = game.images.get("playerLeft");
-	playerLeft = new Splat.AnimatedEntity(50, canvas.height / 2  - playerLeftImg.height / 2, playerLeftImg.width, playerLeftImg.height, playerLeftImg, 0, 0);
-*/
-}, function(elapsedMillis) {
-	/*playerLeft.move(elapsedMillis);
-
-	playerRight.move(elapsedMillis);
-*/
-	/*if(waitingToStart) {
-		var startTimer = this.timer("start");
-		this.camera.vy = player.vy;
-		player.vy = 1;
-		if(startTimer > 100 && anythingWasPressed()){
-			//game.sounds.play("music", true);
-			this.stopTimer("start");
-			startPos = player.y;
-			waitingToStart = false;
+function checkCollisions(row,column){
+	entity = new Splat.AnimatedEntity(0, 0, 100, 140, game.images.get("square0"), 0, 0);
+	entity.x = column *100;
+	entity.y = (row*100) - 40;
+	for (var i = 0; i < balls.length; i++)  {
+		if(balls[i].collides(entity) && balls[i].vy < 0){
+			balls[i].vy *= -1;
+			balls[i].resolveCollisionWith(entity);
+			score += 1;
 		}
-
-	}*/
-/*
-	if (game.keyboard.isPressed("up") && playerRight.y > 0) {
-		playerRight.vy = -0.7;
 	}
-*/
+	//timer?
+}
+
+function addBall(){
+	var ballImg = game.images.get("ball");
+	//context.drawImage(game.images.get("ball"),canvas.width / 2 - 15,canvas.height - 30);	
+	var ball = new Splat.AnimatedEntity(canvas.width / 2 - ballImg.width, canvas.height -50, ballImg.height, ballImg.width, ballImg, 0, 0);
+	ball.vx = .5;
+	ball.vy = -.4;
+	balls.push(ball);
+}
+
+game.scenes.add("title", new Splat.Scene(canvas, function() {
+	balls = [];
+	gameOver = false;
+	addBall();
+	setHeights();
+}, function(elapsedMillis) {
 
 checkKB();
 
-/*for (var i =0; i < row1.length - 1; i++) {
-	if(row1[i] != 1 && index)
-	{
-			//just use i as index goddamn wtf
-			//check keyboard.isPressed(key1 value) if true, change that index in row1 to 1
+for (var i = 0; i < balls.length; i++){
+	var b = balls[i];
+	b.move(elapsedMillis);
+	if(b.x + b.width >= canvas.width){
+		b.vx *= -1;
+	}if(b.x <= 0){
+		b.vx *= -1;
+	}if(b.y + b.height >= canvas.height){
+		b.vy *= -1;
+	}if(b.y < 0){
+		gameOver = true;
 	}
-}*/
+}
+
+if(gameOver){
+	addBall();
+}
 
 }, function(context) {
 	//draw background gradient
 	this.camera.drawAbsolute(context, function() {
-		context.fillStyle="white";
+		context.fillStyle="black";
 		context.fillRect(0,0,canvas.width,canvas.height);
 	});
-/*
-	playerLeft.draw(context); //draw player
-*/
 
 	var keyX = 0;
 	for (var i =0; i < row1.length; i++) {
-		//console.log(row1[i].Counted);
-		context.fillStyle="red";
-		//drawTiles(context);
-
 			this.camera.drawAbsolute(context, function(){
 				//context.fillStyle="green";
 				//context.fillRect(25 + keyX, 100, 100, 100);
-				context.drawImage(game.images.get("square"+row1[i]),25 +keyX, 100 - (10*row1[i]));
+				context.drawImage(game.images.get("square"+row1[i]),keyX, 100 - (10*row1[i]));
 				context.fillStyle="black";
 				context.font = "30px arial"
 				context.fillText(keys1[i], 50 + keyX, 130)
-
-		});
+			});
 			this.camera.drawAbsolute(context, function(){
 				context.fillStyle="green";
 				//context.fillRect(25 + keyX, 200, 100, 100);
-				context.drawImage(game.images.get("square"+row2[i]),25 +keyX, 200 - (10*row2[i]));
+				context.drawImage(game.images.get("square"+row2[i]),keyX, 200 - (10*row2[i]));
 				context.fillStyle="black";
 				context.font = "30px arial"
 				context.fillText(keys2[i], 50 + keyX, 230)
@@ -117,7 +141,7 @@ checkKB();
 			this.camera.drawAbsolute(context, function(){
 				context.fillStyle="green";
 				//context.fillRect(25 + keyX, 300, 100, 100);
-				context.drawImage(game.images.get("square"+row3[i]),25 +keyX, 300 - (10*row3[i]));
+				context.drawImage(game.images.get("square"+row3[i]),keyX, 300 - (10*row3[i]));
 				context.fillStyle="black";
 				context.font = "30px arial"
 				context.fillText(keys3[i], 50 + keyX, 330)
@@ -125,12 +149,27 @@ checkKB();
 		
 		keyX += 100;
 	};
-	
+
+	for (var i = 0; i < balls.length; i++) {
+		if (!balls[i].counted) {
+			balls[i].draw(context);		//draw walls 
+		} else {
+			pickups.splice(pickups[i], 1);
+		}
+	}
+
+	if(gameOver){
+		//this.camera.drawAbsolute(context, function() {
+			context.fillStyle = "#ffffff";
+			context.font = "190px arial";
+			context.fillText("Game Over", 0, canvas.height / 2 + 100);
+		//});
+	}
 
 	this.camera.drawAbsolute(context, function() {
 			context.fillStyle = "#ffffff";
-			context.font = "100px arial";
-			//context.fillText(score, canvas.width / 2, 100);
+			context.font = "50px arial";
+			context.fillText(score, canvas.width / 50, 50);
 	});
 
 }));
